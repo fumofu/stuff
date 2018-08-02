ff8c0652939220401d320b79c4795f8436e20e64
Gabriel Kwan <gabrielk@live.ca>
2018-08-01 17:28:43 -0700

    First commit

---
+++ output.sh
@@ -0,0 +1,21 @@
+# Script that passes outputs from xrandr to dmenu for presentation 
+#dmenu="dmenu -i -b -fn 'Droid Sans Mono-12' -p"
+
+displays=$(xrandr | grep -w "connected" | awk '{print $1}')
+# Checks to see if input is empty, exits with error if true
+[[ "$displays" = "" ]] && exit 1
+chosen=$(echo "$displays"| dmenu -i -p "Which Display?"| awk '{print $1}')
+position=$(echo -e "normal\nleft\nright\noff" | dmenu -i -p "$chosen:")
+if [ "$position" = "" ]  
+then 
+	exit 1
+elif [ "$position" = "off" ]
+then
+	xrandr --output $chosen --$position 
+	notify-send -t 30 "$chosen is no longer outputed" 
+	exit 0
+else
+	xrandr --output $chosen --$position-of eDP-1 --auto  
+	notify-send -t 50 "$chosen is displayed $position" 
+	exit 0 
+fi 
